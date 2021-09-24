72141e2fe668ae4ef0b8d562787f97a9309ac72a
Soham Parekh <soham@Sohams-MacBook-Pro.local>
2021-09-24 17:59:04 +0100

    fix: urdu

--- dataset_gen/urdu_ser.py
+++ dataset_gen/urdu_ser.py
@@ -28,20 +28,21 @@ def main():
         convert_fs(
             os.path.join(data_path, "URDU-Dataset-master"),
             os.path.join(data_path, f"wav{fs // 1000}"),
+            fs
         )
 
     # Create npz files
     for fs in fs_list:
-        src_path = os.path.join(data_path, "URDU-Dataset-master", f"wav{fs // 1000}")
+        src_path = os.path.join(data_path, f"wav{fs // 1000}")
 
-        create_dataset(src_path, os.path.join(src_path, f"wav{fs // 1000}.npz"))
+        create_dataset(src_path, os.path.join(data_path, f"wav{fs // 1000}.npz"))
 
 
 def convert_fs(src_path, dst_path, fs):
-    print("* {src_path} -> {dst_path}")
+    print(f"* {src_path} -> {dst_path}")
     os.mkdir(dst_path)
-    for src_file in sorted(glob.glob(os.path.join(src_path, "**/*.wav"))):
-        dst_file = src_file.replace(src_path, dst_path)
+    for src_file in sorted(glob.glob(os.path.join(src_path, "**", "*.wav"))):
+        dst_file = src_file.replace(os.path.dirname(src_file), dst_path)
         subprocess.call(
             f"ffmpeg -i {src_file} -ac 1 -ar {fs} -loglevel error -y {dst_file}",
             shell=True,
@@ -53,22 +54,20 @@ def create_dataset(src_path, dst_path):
     classes = {"A": 0, "H": 1, "S": 2, "N": 3}
     dataset = {}
 
-    for fold in range(1, 6):
-        dataset[f"fold{fold}"] = {}
-        sounds = []
-        labels = []
+    sounds = []
+    labels = []
 
-        for wav_file in sorted(glob.glob(os.path.join(src_path, "**/*.wav"))):
-            sound = wavio.read(wav_file).data.T[0]
-            start = sound.nonzero()[0].min()
-            end = sound.nonzero()[0].max()
-            sound = sound[start : end + 1]
-            label = classes.get(os.path.splitext(wav_file)[0].split("_")[-1][0])
-            sounds.append(sound)
-            labels.append(label)
+    for wav_file in sorted(glob.glob(os.path.join(src_path, "**", "*.wav"))):
+      sound = wavio.read(wav_file).data.T[0]
+      start = sound.nonzero()[0].min()
+      end = sound.nonzero()[0].max()
+      sound = sound[start : end + 1]
+      label = classes.get(os.path.splitext(wav_file)[0].split("_")[-1][0])
+      sounds.append(sound)
+      labels.append(label)
 
-        dataset[f"fold{fold}"]["sounds"] = sounds
-        dataset[f"fold{fold}"]["labels"] = labels
+      dataset["sounds"] = sounds
+      dataset["labels"] = labels
 
     np.savez(dst_path, **dataset)
 

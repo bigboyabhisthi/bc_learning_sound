8041e038a825c794114c2a9149c9e5cfd38f33ef
meehawk <80167324+meehawk@users.noreply.github.com>
2021-09-26 15:25:29 +0100

    feat: add checkpoints

--- main.py
+++ main.py
@@ -4,9 +4,11 @@
 
 """
 
+import json
 import sys
 import os
 import chainer
+from pathlib import Path
 
 import opts
 import models
@@ -24,6 +26,19 @@ def main():
 
 def train(opt, split):
     model = getattr(models, opt.netType)(opt.nClasses)
+    
+    start_epoch = 0
+    model_root_dir = Path(opt.save)
+    if "model_split_{}.npz".format(split) in [f for f in os.listdir(model_root_dir)]:
+        print("Pre-trained available for model_split_{}.npz".format(split))
+        chainer.serializers.load_npz(
+            model_root_dir / "model_split_{}.npz".format(split), model
+        )
+        with open(model_root_dir / "epoch_{}.json".format(split), "r") as fp:
+            start_epoch = json.load(fp)["epoch"]
+
+    print("Starting training at epoch: {}".format(start_epoch + 1))
+
     model.to_gpu()
     optimizer = chainer.optimizers.NesterovAG(lr=opt.LR, momentum=opt.momentum)
     optimizer.setup(model)
@@ -39,7 +54,7 @@ def train(opt, split):
         print("| Val: top1 {:.2f}".format(val_top1))
         return
 
-    for epoch in range(1, opt.nEpochs + 1):
+    for epoch in range(start_epoch + 1, opt.nEpochs + 1):
         train_loss, train_top1 = trainer.train(epoch)
         val_top1 = trainer.val()
         sys.stderr.write("\r\033[K")
@@ -53,6 +68,14 @@ def train(opt, split):
                 val_top1,
             )
         )
+
+        if epoch % 50 == 0:
+            chainer.serializers.save_npz(
+                model_root_dir / "model_split_{}.npz".format(split), model
+            )
+            with open(model_root_dir / "epoch_{}.json".format(split), "w") as fp:
+                json.dump({"epoch": epoch}, fp)
+        
         sys.stdout.flush()
 
     if opt.save != "None":
